8f764faf5d67dbc192ee7a5a71f17bee02070cf6
ShahidaJubil <shahidajubil@gmail.com>
2025-11-03 17:40:20 +0530

    Error correction

--- Frontend/employee-id-frontend/src/api.js
+++ Frontend/employee-id-frontend/src/api.js
@@ -1,4 +1,11 @@
+// import axios from "axios";
+// export default axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
 import axios from "axios";
+
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://127.0.0.1:5000/api",   // use IPv4 loopback
+  headers: { "Content-Type": "application/json" },
 });
